656e7f1a26a588abfef02b4e47f9b5e9eeb1d3e4
Masqurin <Masqurin@icloud.com>
2017-07-21 11:35:51 +0800

    3建立管理者及建立delegate

--- HelloMyBLE/CentralTableViewController.m
+++ HelloMyBLE/CentralTableViewController.m
@@ -25,6 +25,7 @@
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    manager = [[CBCentralManager alloc] initWithDelegate:self queue:nil];
 }
 
 - (void)didReceiveMemoryWarning {
