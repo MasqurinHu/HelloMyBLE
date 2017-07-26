b17726eb24fb343555a778bce3da000043f93136
Masqurin <Masqurin@icloud.com>
2017-07-26 11:07:36 +0800

    把talking的銜接事項準備「只有外殼」
    Navi 不能放東西
    因為沒有navi item 加了就可以方了

---
+++ HelloMyBLE/TalkingViewController.m
@@ -0,0 +1,50 @@
+//
+//  TalkingViewController.m
+//  HelloMyBLE
+//
+//  Created by Ｍasqurin on 2017/7/26.
+//  Copyright © 2017年 Ｍasqurin. All rights reserved.
+//
+
+#import "TalkingViewController.h"
+#import "MUIBottonlineTextField.h"
+
+@interface TalkingViewController ()
+
+
+@property (weak, nonatomic) IBOutlet MUIBottonlineTextField *inputTextField;
+@property (weak, nonatomic) IBOutlet UITextView *logTextView;
+
+
+
+
+@end
+
+@implementation TalkingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)sendBtnPressed:(id)sender {
+    
+}
+
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
