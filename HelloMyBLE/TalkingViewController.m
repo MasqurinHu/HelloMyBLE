f6c6e41b0c0e10c9369e2ae83e7c4494727ca291
Masqurin <Masqurin@icloud.com>
2017-07-26 11:35:43 +0800

    重裝置讀取資料

--- HelloMyBLE/TalkingViewController.m
+++ HelloMyBLE/TalkingViewController.m
@@ -9,7 +9,7 @@
 #import "TalkingViewController.h"
 #import "MUIBottonlineTextField.h"
 
-@interface TalkingViewController ()
+@interface TalkingViewController ()<CBPeripheralDelegate>
 
 
 @property (weak, nonatomic) IBOutlet MUIBottonlineTextField *inputTextField;
@@ -25,6 +25,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    //通訊工作前置準備
+    
+    //ble 可以讀寫資料 可能讀不出資料
+    CBPeripheral *peripheral = _targetCharacteristic.service.peripheral;
+    peripheral.delegate = self;
+    //所以設定 訂閱模式 東西有資料主動通知
+    [peripheral setNotifyValue:true forCharacteristic:_targetCharacteristic];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,6 +41,20 @@
 
 - (IBAction)sendBtnPressed:(id)sender {
     
+}
+
+#pragma mark - CBPeripheralDelegate
+//接收訂閱通知 
+-(void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error{
+    
+    NSString *message;
+    if (error) {
+        message = error.description;
+    }else{
+        message = [[NSString alloc] initWithData:characteristic.value encoding:NSUTF8StringEncoding];
+    }
+    
+    _logTextView.text = [NSString stringWithFormat:@"%@%@",message,_logTextView.text];
 }
 
 
